bf37c50e5bea007f254f9219bcaaadf44a772076
Henry Avink <henryscomputerscience@gmail.com>
2022-03-11 09:45:00 -0500

    Added story in a print statement - Stable

---
+++ MidTermExam.py
@@ -0,0 +1,1 @@
+print("Once apon a time I built my second ever computer.\nIt is way better than the first one I build and I use it almost every day.\nWhile I was building it, I got stuck a few times but in the end I got it finished and all working.\nI now use it to run game on my monitors and on my VR headset, the Valve Index.\nIt works really well.")
